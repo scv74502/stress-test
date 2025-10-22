d08a5eedda797a34510acbed4897684e5abbe0e1
kunwoo park <scv74502@gmail.com>
2025-10-22 23:34:36 +0900

    feat: JPA 및 DB 커넥션 설정 라인 추가(TBD), KSP 어노테이션 프로세서와 QueryDSL 설정 라인 추가(TBD), Spring boot 3의 Kotlin 관련 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,36 @@
+import org.gradle.kotlin.dsl.withType
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
-    id("org.springframework.boot") version "3.2.0"
-    id("io.spring.dependency-management") version "1.1.4"
-    kotlin("jvm") version "1.9.22"
-    kotlin("plugin.spring") version "1.9.22"
+    id("org.springframework.boot") version "3.5.6"
+    id("io.spring.dependency-management") version "1.1.7"
+    // KSP annotation processor
+//    id("com.google.devtools.ksp") version "1.9.25-1.0.20"
+    kotlin("jvm") version "1.9.25"
+    kotlin("plugin.spring") version "1.9.25"
+//    kotlin("plugin.jpa") version "1.9.25"
 }
 
 group = "com.example"
 version = "0.0.1-SNAPSHOT"
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_21
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+// kotlin 컴파일 시 옵션
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "21"
+    }
+}
+
+// junit 테스트 시 사용하는 설정
+tasks.withType<Test> {
+    useJUnitPlatform()
 }
 
 repositories {
@@ -17,13 +38,24 @@ repositories {
 }
 
 dependencies {
+    // 코틀린에 필요한 기존 의존성
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+    // JPA 및 DB 연결 설정
+//    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+//    runtimeOnly("mysql:mysql-connector-java:8.0.33")
+    // QueryDSL
+//    implementation("io.github.openfeign.querydsl:querydsl-jpa:7.0")
+
+    // KSP를 사용한 querydsl 어노테이션 프로세서 추가
+//    ksp("io.github.openfeign.querydsl:querydsl-ksp-codegen:7.0")
+
+    // .env 파일 지원
+    implementation("me.paulschwarz:spring-dotenv:4.0.0")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-}
-
-
-tasks.withType<Test> {
-    useJUnitPlatform()
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
