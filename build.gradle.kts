5c41e1f8682ffca1523f7ef5d5a67edf00495488
kunwoo park <scv74502@gmail.com>
2025-10-25 01:29:30 +0900

    feat: 의존성 활성화

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,10 +5,10 @@ plugins {
     id("org.springframework.boot") version "3.5.6"
     id("io.spring.dependency-management") version "1.1.7"
     // KSP annotation processor
-//    id("com.google.devtools.ksp") version "1.9.25-1.0.20"
+    id("com.google.devtools.ksp") version "1.9.25-1.0.20"
     kotlin("jvm") version "1.9.25"
     kotlin("plugin.spring") version "1.9.25"
-//    kotlin("plugin.jpa") version "1.9.25"
+    kotlin("plugin.jpa") version "1.9.25"
 }
 
 group = "com.example"
@@ -37,20 +37,23 @@ repositories {
     mavenCentral()
 }
 
+val queryDslVersion = "7.0"
+
 dependencies {
     // 코틀린에 필요한 기존 의존성
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.springframework.boot:spring-boot-starter-validation")   // validation
 
     // JPA 및 DB 연결 설정
-//    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-//    runtimeOnly("mysql:mysql-connector-java:8.0.33")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    runtimeOnly("com.mysql:mysql-connector-j")
     // QueryDSL
-//    implementation("io.github.openfeign.querydsl:querydsl-jpa:7.0")
+    implementation("io.github.openfeign.querydsl:querydsl-jpa:${queryDslVersion}")
 
     // KSP를 사용한 querydsl 어노테이션 프로세서 추가
-//    ksp("io.github.openfeign.querydsl:querydsl-ksp-codegen:7.0")
+    ksp("io.github.openfeign.querydsl:querydsl-ksp-codegen:${queryDslVersion}")
 
     // .env 파일 지원
     implementation("me.paulschwarz:spring-dotenv:4.0.0")
