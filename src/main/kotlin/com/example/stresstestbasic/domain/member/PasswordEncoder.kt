d30e2beb42dfa16a31c53255a8b0d9b897a5fd4e
kunwoo park <scv74502@gmail.com>
2025-10-25 01:32:14 +0900

    feat: PasswordEncoder interface

---
+++ src/main/kotlin/com/example/stresstestbasic/domain/member/PasswordEncoder.kt
@@ -0,0 +1,6 @@
+package com.example.stresstestbasic.domain.member
+
+interface PasswordEncoder {
+    fun encode(password: String): String
+    fun matches(password: String, passwordHash: String): Boolean
+}
